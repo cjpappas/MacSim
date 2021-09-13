b42ea610a4760f2ed0d00b519e79e56a7cd63fd2
Matt Roberts <matthew.roberts@mq.edu.au>
2021-09-13 16:13:59 +1000

    removing things I don't need.  Renaming other things.  Added hostnames so we don't rely on IP addresses in scripts

--- catkin_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/catkin_ws/src
-# Build directory: /home/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("demo")
--- catkin_ws/build/demo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/catkin_ws/src/demo
-# Build directory: /home/catkin_ws/build/demo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
