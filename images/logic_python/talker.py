8b762367c9bbc8efedc19d8b3b74dc0eb2cddb24
Matt Roberts <matthew.roberts@mq.edu.au>
2021-09-15 08:21:28 +1000

    three nodes all working together now, including a headless simulation. Data is being passed around as we want.

--- images/logic_python/talker.py
+++ images/logic_python/talker.py
@@ -7,11 +7,13 @@ print("master instead of IP")
 client = roslibpy.Ros(host='master', port=9090)
 client.run()
 
+print(client.get_topics())
+
 talker = roslibpy.Topic(client, '/chatter', 'std_msgs/String')
 
 while client.is_connected:
     talker.publish(roslibpy.Message({ 'data': 'Hello World!' }))
-    print('Sending message...')
+    # print('Sending message...')
     time.sleep(1)
 
 talker.unadvertise()
