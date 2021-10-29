5fdfc0e4482f3020636104b022bcdecf8aa23adb
Cameron Pappas <cameron.pappas@mq.edu.au>
2021-10-29 16:53:47 +1100

    Add documentation to api

--- local_programs/node/Main.js
+++ local_programs/node/Main.js
@@ -3,16 +3,19 @@ const { init } = require("./Api");
 // Returns a connection to the rosbridge server
 const ros = init("ws://172.17.0.3:9090");
 const stationKeeping = () => {
-    if(ros.data.goal.error > 4){
-        const r = Math.atan2(ros.data.current.lng - ros.data.goal.lng, ros.data.current.lat - ros.data.goal.lat);
-        console.log(r - ros.data.current.direction);
-        if(r - ros.data.current.direction < 3){
-            ros.moveForward();
-        } else {
-            ros.rotateLeft();
-        }
-    } else {
-        ros.stop();
-    }
+    // const goal = ros.getGoalPosition();
+    // const pos = ros.getPosition();
+    // if(goal.err > 4){
+    //     const r = Math.atan2(pos.lng - goal.lng, pos.lat - goal.lat);
+    //     if(r - pos.dir < 3){
+    //         ros.moveForward();
+    //     } else {
+    //         ros.rotateLeft();
+    //     }
+    // } else {
+    //     ros.stop();
+    // }
+    console.log(ros.getTask());
+    console.log(ros.getGoalPosition());
 }
 ros.execute(stationKeeping, 1000);
