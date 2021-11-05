a685b5fa1563f8b0e2972c129a5cbf2bf1d50fe6
Matthew Roberts <matthew.roberts@mq.edu.au>
2021-11-05 16:44:41 +1100

    minor station-keeping improvments

--- images/sim/html/api.js
+++ images/sim/html/api.js
@@ -4,10 +4,11 @@ const { WebSocketSubject } = rxjs.webSocket;
 master = new WebSocketSubject("ws://localhost:9090");
 master.subscribe(
   msg => {
+    document.getElementById("error").innerHTML = "";
     sinks[msg.topic](msg);
   },
-  err => console.log("ERROR: " + err),
-  () => console.log("COMPLETE")
+  err => {console.log("ERROR: " + err); document.getElementById("error").innerHTML = "Connection Error";},
+  () => {console.log("COMPLETE"); document.getElementById("error").innerHTML = "Connection Lost";}
 );
 
 function subscribe(topic, action) {
@@ -21,25 +22,17 @@ function toDegs(degrees){
 function invert(rads){
   return -1*rads
 }
-function iconFromHeading(rotation){
-  return {path: "M -12 5 L -12 -5 L 0 -5 L 0 -18 L 37 0 L 0 18 L 0 5 Z", 
-          fillOpacity: 1,
-          fillColor: "#FFFFFF",
-          scale:0.5, 
-          rotation: invert(toDegs(rotation))
-          }
-}
 function imuMsgToHeading(msg){
   var eu = new THREE.Euler();
   var ex = new THREE.Quaternion(msg.msg.orientation.x, msg.msg.orientation.y, msg.msg.orientation.z, msg.msg.orientation.w);
   eu.setFromQuaternion(ex)
   return eu.z
 }
-function icon(rotation){
+function icon(rotation, colour, scale){
   return {path: "M -12 5 L -12 -5 L 0 -5 L 0 -18 L 37 0 L 0 18 L 0 5 Z", 
           fillOpacity: 1,
-          fillColor: "#FFFFFF",
-          scale:0.5, 
+          fillColor: colour,
+          scale:0.5*scale, 
           rotation:(rotation/(Math.PI))*-180
         }
 }
@@ -64,4 +57,8 @@ function rotateAntiClockwise(strength){
 function rotateClockwise(strength){
   leftThrust(strength, 2000);
   rightThrust(-1*strength, 2000);
+}
+function forward(strength){
+  leftThrust(strength, 1000);
+  rightThrust(strength, 1000);
 }
