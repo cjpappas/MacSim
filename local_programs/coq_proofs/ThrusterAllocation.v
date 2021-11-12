571857cfbfbd722c6c81969cd7b0210fa349fb94
Cameron Pappas <cameron.pappas@mq.edu.au>
2021-11-12 16:01:13 +1100

    Remove ununsed programs and images

--- local_programs/coq_proofs/ThrusterAllocation.v
+++
@@ -1,7 +0,0 @@
-From mathcomp Require Import all_ssreflect all_algebra.
-Require Extraction.
-Extraction Language OCaml.
-
-Definition compute_thrust (nd : 'M[nat]_(1,3)) : nat := 1.
-
-Extraction "ThrusterAllocation.ml" compute_thrust.
--- local_programs/node/Main.js
+++
@@ -1,9 +0,0 @@
-const { init } = require("./api");
-
-// Returns a connection to the rosbridge server
-const ros = init("ws://172.17.0.2:9090");
-setInterval(() => {
-    ros.moveForward(100)
-        .then(() => ros.rotateLeft(100))
-        .catch((error) => console.log("Error: " + JSON.stringify(error)));
-}, 5000);
