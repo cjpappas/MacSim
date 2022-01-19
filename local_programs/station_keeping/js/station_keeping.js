28d0eea7f82ab4fdafedc61d2c194f19c1deb8e6
Matthew Roberts <matthew.roberts@mq.edu.au>
2022-01-19 14:01:56 +1100

    moving javascript station keeper in the right direction

--- local_programs/station_keeping/js/station_keeping.js
+++ local_programs/station_keeping/js/station_keeping.js
@@ -3,11 +3,36 @@ console.log(init);
 
 const craft = init("localhost");
 
-craft.moveForward(1);
-let inter = setInterval(() => {
-  craft.rotateAnticlockwise(10);
-}, 1000);
+class Polar{
+  constructor(psi, mag){
+    this.psi = psi;
+    this.mag = mag;
+  }
 
-setTimeout(() => {
-  clearInterval(inter);
-}, 10000);
+  plus(other){
+    return new Polar( this.psi + atan2(other.mag*sin(other.psi - this.psi), this.mag + other.mag*cos(other.psi-this.psi))
+    , sqrt(other.mag*other.mag + this.mag*this.mag + 2*this.mag*other.mag*cos(other.psi-this.psi))
+    );
+  }
+
+  mult(other){
+    return new Polar(this.psi, this.mag*amt);
+  }
+
+  minus(other){
+    return this.plus(other.mult(-1));
+  }
+}
+
+let craftPos = new Polar(0,10);
+
+function draw(){
+  craft.log(craft.getPosition());
+  // craft.log(craft.getVelocity());
+  craft.moveForward(1);
+
+}
+
+let inter = setInterval(draw, 1000);
+
+setTimeout(() => {clearInterval(inter), exit();}, 50000);
