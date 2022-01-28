d33e24ed01791e606e716778b73ea92cfbae9020
Cameron Pappas <cameron.pappas1@gmail.com>
2022-01-28 11:59:51 +1100

    Update realtive paths in dockerfile and server to be full paths

--- images/base/html/server.py
+++ images/base/html/server.py
@@ -37,13 +37,13 @@ def start():
     if sim in sims.keys():
         # Need a virtual display to force gazebo to add Camera sensors and publish
         # https://github.com/PX4/PX4-containers/issues/198
-        subprocess.run(["source ~/vrx_ws/devel/setup.bash && (Xvfb :1 -screen 0 1600x1200x16 &) && export DISPLAY=:1.0 && roslaunch vrx_gazebo %s gui:=false &"%(sims[sim])],
+        subprocess.run(["source /home/developer/vrx_ws/devel/setup.bash && (Xvfb :1 -screen 0 1600x1200x16 &) && export DISPLAY=:1.0 && roslaunch vrx_gazebo %s gui:=false &"%(sims[sim])],
                                  executable="/bin/bash", shell=True)
         return {"status": "Simulation started."}
     return {"status": "Simulation type not found."}
 
 @app.route("/api/stop_sim", methods=["POST"])
 def stop():
-    subprocess.run(["source ~/vrx_ws/devel/setup.bash && rosnode kill gazebo && killall -9 gzserver"],
+    subprocess.run(["source /home/developer/vrx_ws/devel/setup.bash && rosnode kill gazebo && killall -9 gzserver"],
                     executable="/bin/bash", shell=True)
     return {}, 200
