498a968b831d6bd9af1cb676003dddcd5f752454
Cameron Pappas <cameron.pappas1@gmail.com>
2022-02-10 16:58:01 +1100

    Add demo to submission to test

--- images/submission/src/main.js
+++ images/submission/src/main.js
@@ -1,1 +1,7 @@
-console.log("main.js");
+const { init, startSim, stopSim } = require("./api");
+
+const setup = () => console.log("Setup complete");
+
+const act = () => setInterval(() => craft.log(craft.getPosition()), 4000);
+
+const craft = init("localhost", setup, act);
