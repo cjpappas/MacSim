62f8f2020e7caf73378bf0de42d626850bc7a172
Matt Roberts <matthew.roberts@mq.edu.au>
2022-03-12 20:22:31 +1100

    first draft of a main.js file for the competition submission.

--- images/submission/src/main.js
+++ images/submission/src/main.js
@@ -1,7 +1,54 @@
 const { init } = require("./api");
 
-const setup = () => console.log("Setup complete");
+const craft = init(process.env.IP);
 
-const act = () => setInterval(() => craft.log(craft.getTaskInfo()), 4000);
+while (true){
+  if(craft.data.task.state === "running"){
+    switch(craft.data.task.name){
+      case "station_keeping":   while_running(station_keeping, craft);   break;
+      case "wayfinding":        while_running(wayfinding, craft);        break;
+      case "perception":        while_running(perception, craft);        break;
+      case "wildlife":          while_running(wildlife, craft);          break;
+      case "gymkhana":          while_running(gymkhana, craft);          break;
+      case "scan_dock_deliver": while_running(scan_dock_deliver, craft); break;
+      
+    }
+  }
+}
 
-const craft = init(process.env.IP, setup, act);
+const while_running = (fn, craft) => {
+  console.log(fn.name + " has been summoned");
+  while(craft.data.task.state === running){
+    fn(craft);
+  }
+}
+
+const station_keeping = (craft) => {
+  console.log("inspecting");
+  console.log("deciding");
+}
+
+const wayfinding = (craft) => {
+  console.log("inspecting");
+  console.log("deciding");
+}
+
+const perception = (craft) => {
+  console.log("inspecting");
+  console.log("deciding");
+}
+
+const wildlife = (craft) => {
+  console.log("inspecting");
+  console.log("deciding");
+}
+
+const gymkhana = (craft) => {
+  console.log("inspecting");
+  console.log("deciding");
+}
+
+const scan_dock_deliver = (craft) => {
+  console.log("inspecting");
+  console.log("deciding");
+}
